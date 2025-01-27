4533c1348ead038e492e996adeb7e39a066fa629
Ina-Ango <georgepenney02@gmail.com>
2025-01-26 19:38:10 -0500

    I have restaged this page to be built in react

---
+++ ina-wiki/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Work in progress wiki for the Ina universe
+        </p>
+        <a
+          className="App-link"
+          href="https://x.com/Ina_den_"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Inanic Wiki zone
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
